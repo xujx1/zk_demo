31dbe7e4e4569050f720c2f7325805f37ca287b2
xujinxin <153268908@qq.com>
2017-07-21 10:35:46 +0800

    master选举

--- src/main/java/com/zk/demo/_4master/_2curator/LeaderSelectCuratorClient.java
+++ src/main/java/com/zk/demo/_4master/_2curator/LeaderSelectCuratorClient.java
@@ -8,9 +8,11 @@ import org.apache.curator.framework.recipes.leader.LeaderSelector;
 import org.apache.curator.framework.recipes.leader.LeaderSelectorListener;
 import org.apache.curator.framework.state.ConnectionState;
 import org.apache.curator.retry.ExponentialBackoffRetry;
-import org.apache.curator.utils.EnsurePath;
 
 import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by xujinxin on 2017/7/20.
@@ -18,26 +20,34 @@ import java.util.Arrays;
  */
 public class LeaderSelectCuratorClient {
 
-    private static final String MASTER_PATH = "/master";
-    private static final String ZK_PATH = "127.0.0.1:2181";
+    private static final String ZK_ADDRESS = "127.0.0.1:2181";
+    private static final String MASTER = "/master";
+    private static final CountDownLatch COUNT_DOWN_LATCH = new CountDownLatch(1);
+    private static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(3);
 
 
+    //测试
     public static void main(String[] args) throws InterruptedException {
-        CuratorFramework curatorFramework =
-                CuratorFrameworkFactory.builder()
-                        .connectString(ZK_PATH)
-                        .retryPolicy(new ExponentialBackoffRetry(1000, 3))
-                        .build();
+        final LeaderSelectorListener listener = initListener();
+
+        for (int i = 0; i < 3; i++) {
+            EXECUTOR_SERVICE.submit(() -> registerListener(listener));
+        }
+
+        //让主线程不中断
+        COUNT_DOWN_LATCH.await();
+    }
 
 
-        curatorFramework.start();
+    /**
+     * LeaderSelectorListener可以对领导权进行控制， 在适当的时候释放领导权，这样每个节点都有可能获得领导权。
+     */
+    private static LeaderSelectorListener initListener() {
 
-        LeaderSelector selector = new LeaderSelector(curatorFramework, MASTER_PATH, new LeaderSelectorListener() {
+        return new LeaderSelectorListener() {
             @Override
             public void takeLeadership(CuratorFramework curatorFramework) throws Exception {
-                System.out.println("成为master");
-                Thread.sleep(3000);
-                System.out.println("完成master操作，释放权利");
+                System.out.println(Thread.currentThread().getName() + " 成为master");
             }
 
             @Override
@@ -46,25 +56,33 @@ public class LeaderSelectCuratorClient {
                     throw new CancelLeadershipException();
                 }
             }
-        });
+        };
+    }
+
+    /**
+     * 注册选举的监听事件
+     */
+    private static void registerListener(LeaderSelectorListener listener) {
+        CuratorFramework curatorFramework = connection();
+
+        LeaderSelector selector = new LeaderSelector(curatorFramework, MASTER, listener);
         selector.autoRequeue();
         selector.start();
-        Thread.sleep(Integer.MAX_VALUE);
     }
 
 
-    private static void registerListener(LeaderSelectorListener listener) {
+    /**
+     * 建立连接
+     */
+    private static CuratorFramework connection() {
         CuratorFramework curatorFramework =
                 CuratorFrameworkFactory.builder()
-                        .connectString(ZK_PATH)
+                        .connectString(ZK_ADDRESS)
                         .retryPolicy(new ExponentialBackoffRetry(1000, 3))
                         .build();
         curatorFramework.start();
 
-
-        LeaderSelector selector = new LeaderSelector(curatorFramework, ZK_PATH, listener);
-        selector.autoRequeue();
-        selector.start();
+        return curatorFramework;
     }
 
 }
